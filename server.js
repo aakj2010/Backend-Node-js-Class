d549ef0be6b49245852987b9ee016f1fcdd90fc7
aakj2010 <aakj2010@gmail.com>
2022-09-25 16:28:00 +0530

    10

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ let users = [];
 // Middleware
 app.use(express.json())
 app.use(cors({
-    origin : 'http://localhost:3000'
-    // origin:'https://sensational-pegasus-ea84cf.netlify.app'
+    // origin : 'http://localhost:3000'
+    origin:'https://sensational-pegasus-ea84cf.netlify.app'
 }))
 
 
